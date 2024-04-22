f58b7b29a12da9129f2c4c16848b173893b6590d
gruntworks <mateo.cobanov@gmail.com>
2024-04-22 12:58:27 +0200

    Add static files to build

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/m5-memory' : ''
 		}
